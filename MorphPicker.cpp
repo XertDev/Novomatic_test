b87d335f2cd42644852d1a1a225ed6082fd5f687
Xert <michal740@interia.eu>
2020-05-30 22:47:42 +0200

    Readme and code description

--- MorphPicker.cpp
+++ MorphPicker.cpp
@@ -1,37 +1,46 @@
 #include "MorphPicker.hpp"
-#include <numeric>
 
 std::array<int, 4> pickMorphs(const std::vector<float>& weights)
 {
-	std::array<int, 4> retval{};
-	retval.fill(-1);
+	std::array<int, 4> selected{};
+	selected.fill(-1);
 
+	//if less or equal 4 weights fill with all available indices
 	if(weights.size() <= 4)
 	{
-		for(size_t i = 0; i < weights.size(); ++i)
+		for(std::size_t i = 0; i < weights.size(); ++i)
 		{
-			retval[i] = i;
+			selected[i] = i;
 		}
-		return retval;
+		return selected;
 	}
 
-	std::iota(retval.begin(), retval.end(), 0);
-	std::sort(retval.begin(), retval.end(),
-			[&](int i1, int i2) { return weights[i1] < weights[i2];}
+	for(std::size_t i = 0; i < 4; ++i)
+	{
+		selected[i] = i;
+	}
+
+	std::sort(selected.begin(), selected.end(),
+	          [&](int i1, int i2) { return weights[i1] < weights[i2];}
 	);
+
+	//Keep the selected value indices in ascending order by value. While checking new values, compare with the smallest
+	//of selected. If new value is bigger than previous smallest of biggest, put new value as smallest of biggest
+	//and start shifting to valid position.
+	//Computational complexity is O(n), because we do at most 4 shifts for every value.
 
 	for(std::size_t i = 4; i < weights.size(); ++i)
 	{
-		if(weights[retval[0]] < weights[i])
+		if(weights[selected[0]] < weights[i])
 		{
-			retval[0] = i;
+			selected[0] = i;
 			std::size_t curr_ind = 0;
-			while(curr_ind < 3 && weights[retval[curr_ind]] > weights[retval[curr_ind+1]])
+			while(curr_ind < 3 && weights[selected[curr_ind]] > weights[selected[curr_ind + 1]])
 			{
-				std::swap(retval[curr_ind], retval[curr_ind+1]);
+				std::swap(selected[curr_ind], selected[curr_ind + 1]);
 				++curr_ind;
 			}
 		}
 	}
-	return retval;
+	return selected;
 }
